552d496bf9b2660f4d823ba94e29713ccd946c00
quincy <qhutchison@infonaligy.com>
2022-03-27 15:35:27 +0000

    updates to button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,9 +1,13 @@
 import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
 type Props = {
     label: string,
     type: "violet"|"dark"|"dark-light"|"red"|"grey"|"grey-light"
     size: "medium"|"large"
     customClass?: string[],
+    iconClass: string[]
+    icon?: IconProp,
     onClick: (event:any)=>void
 }
 
@@ -11,13 +15,22 @@ class Button extends React.Component<Props> {
     static defaultProps = {
         type: "violet",
         size: "medium",
-        customClasses: []
+        customClasses: [],
+        iconClass: []
       };
     render() {
 
      const className = `text-sm-bold ${this.props.type} ${this.props.size} ${(this.props.customClass || []).join(' ')}`;
-      return( 
-        <button onClick={this.props.onClick} className = {className} >{ this.props.label}</button>
+     
+     let icon = <></>;
+     if(this.props.icon){
+         icon = <FontAwesomeIcon  className={this.props.iconClass.join(' ')} icon={this.props.icon} />
+     }
+     return( 
+        <button onClick={this.props.onClick} className = {className} > 
+        {icon}
+        <span>{ this.props.label}</span>
+        </button>
       );
     }
   }
