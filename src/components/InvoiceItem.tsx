19d1025886fd4a8d01d9ebee21f5b049d0f6ea50
quincy <qhutchison8@gmail.com>
2022-03-29 23:46:24 +0000

    adding item list

--- src/components/InvoiceItem.tsx
+++ src/components/InvoiceItem.tsx
@@ -1,59 +1,32 @@
 import React from 'react';
-import {ReactComponent as ArrowRight} from "../assets/icon-arrow-right.svg";
+import {ReactComponent as Delete} from "../assets/icon-delete.svg";
+import Input from './elements/Input';
 type Props = {
     item: {
-      id: string;
-      paymentDue: string;
-      clientName: string;
-      total: number;
-      status: "paid" | "pending"| "draft"
+      name: string
+      quantity: number,
+      price: number,
   }
 }
 
 class InvoiceItem extends React.Component<Props> {
   static defaultProps = {
     item: {
-      id: "",
-      paymentDue: "",
-      clientName: "",
-      total: 0,
-      status: ""
+      name: "",
+      quantity: 0,
+      price: 0
     }
   };
-  formatDate = (date:string)=> {
-    if(!date) return ""
-    return (new Date(date)).toLocaleDateString("en-US",{month:"short",year:"numeric",day:"numeric"})
-  }
-
-  formatNumber = (price:number)=> {
-    if(!price) return ""
-    const format = Intl.NumberFormat('en-US',{ style: 'currency', currency: 'GBP' }).format
-    return `${format(price)}`;
-  }
-  upperCaseFirst(data:string) {
-    return `${data[0].toUpperCase()}${data.substring(1)}`
-  }
+  
   render() {
     
-    let statusClass = "paid";
-    if(this.props.item.status === 'pending'){
-      statusClass='pending'
-    }else if (this.props.item.status === 'draft'){
-      statusClass = "draft"
-    }
     return (
-      <div className='invoice-item '>
-        <h3 className='text-sm-bold'> <span className='pound'>#</span>{this.props.item.id}</h3>
-        <h3 className='text-sm'> {this.formatDate(this.props.item.paymentDue)}</h3>
-        <h3 className='text-sm'> {this.props.item.clientName}</h3>
-        <h2 > {this.formatNumber(this.props.item.total)}</h2>
-        <div className={`invoice-status ${statusClass}`}>
-          <span> &#8226;</span>
-           <p > {this.upperCaseFirst(this.props.item.status)}</p>
-       </div>
-       <div>
-        <ArrowRight/>
-        </div>
+      <div className='invoice-item'>
+       <Input customClass={["invoice-item-name"]} label={"Item Name"} onInput={()=>console.log('test')} inputType={'text'} />
+       <Input customClass={['invoice-item-qty']} label={"Qty"} onInput={()=>console.log('test')} inputType={'text'} />
+       <Input customClass={['invoice-item-price']} label={"Price"} onInput={()=>console.log('test')} inputType={'text'} />
+        <span className='text-sm'>Total</span>
+        <Delete/>
       </div>
     );
   }
