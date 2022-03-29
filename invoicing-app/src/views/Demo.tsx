84cc2fa210d8a558fc3ad32bad24eb9882cb2ec3
quincy <qhutchison8@gmail.com>
2022-03-29 23:10:50 +0000

    taking out fontawesome, adding redux

--- invoicing-app/src/views/Demo.tsx
+++ invoicing-app/src/views/Demo.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import Button from '../components/elements/Button';
 import Input from '../components/elements/Input';
-import { solid} from '@fortawesome/fontawesome-svg-core/import.macro'
 import Select from "../components/elements/Select"
 class Demo extends React.Component {
   render(){
     return (
         <div >
-          <Button icon={solid('plus-circle' )}label={"New Invoice"} onClick={()=>console.log('test')} type= {"grey"} size={"large"} />
+          <Button icon="plus" label={"New Invoice"} onClick={()=>console.log('test')} type= {"grey"} size={"large"} />
           <Input customClass={["dark"]} value='hello' label={"Add new Item"} onInput={()=>console.log('test')} type= {"grey"} size={"large"} />
           <Input customClass={["dark"]} label={"Add new Item"} onInput={()=>console.log('test')} type= {"grey"} inputType={'date'} size={"large"} />
           <Select customClass={["dark"]} options={[{label:"Volvo",value:"volvo"},{label:"Saab",value:"saab"}]} label={"Add new Item"} onChange={()=>console.log('test')}  />
