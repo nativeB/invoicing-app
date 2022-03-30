7d411245691728b13d94972586f22a0a4cea3b3b
quincy <qhutchison8@gmail.com>
2022-03-30 00:04:41 +0000

    invoice reducer

---
+++ src/store/features/invoice/index.ts
@@ -0,0 +1,32 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+export const InvoiceSlice = createSlice({
+  name: 'app',
+  initialState: {
+    data: [],
+  },
+  reducers: {
+    setData: (state, action) => {
+      state.data = action.payload
+    },
+    setOneInvoice: (state, action) => {
+      switch(action.payload.type) {
+        case "add":
+          state.data = [...state.data, action.payload] as any
+          break;
+        case "update":
+          const data:any = state.data.map((invoice:any) => {
+            if(invoice.id === action.payload.id) {
+              return action.payload
+            }
+            return invoice
+          })
+          state.data = data
+    }
+  },
+}
+})
+
+export const { setData, setOneInvoice } = InvoiceSlice.actions
+
+export default InvoiceSlice.reducer
