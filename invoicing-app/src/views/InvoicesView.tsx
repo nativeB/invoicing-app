c2b60940042989d9d8e29dce9afb409e70f16611
quincy <qhutchison8@gmail.com>
2022-04-01 09:25:46 +0000

    add dialog for delete feature implementation

--- invoicing-app/src/views/InvoicesView.tsx
+++ invoicing-app/src/views/InvoicesView.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import Dialog from '../components/dialog/Dialog';
 import Button from '../components/elements/Button';
 import InvoiceHeader from '../components/InvoiceHeader';
 import InvoiceStatusTag from '../components/InvoiceStatusTag';
@@ -16,13 +17,17 @@ type Prop = {
     setOneInvoice: (invoiceItem:any) => void;
 }
 type State = {
-  invoice: any;
+  showDeleteDialog: boolean;
 }
 
 class InvoiceView extends React.Component<Prop, State> {
   constructor(props: Prop) {
     super(props);
+    this.state = {
+      showDeleteDialog: false
+    }
     this.markAsPaid = this.markAsPaid.bind(this);
+    this.setShowDeleteDialog = this.setShowDeleteDialog.bind(this);
   }
 
   static defaultProps = {
@@ -36,7 +41,14 @@ class InvoiceView extends React.Component<Prop, State> {
     }
     this.props.setOneInvoice(newInvoice);
   }
-  
+  setShowDeleteDialog(show:boolean){
+    this.setState((prevState:State)=>{
+      return {
+        showDeleteDialog: show
+      }
+    })
+  }
+
   render(){
     const invoice = this.props.getOneInvoice(this.props.params.id)||{};
     const headerLeft = <div className='App-header-status'>
@@ -49,7 +61,7 @@ class InvoiceView extends React.Component<Prop, State> {
         this.props.setEditingInvoice(invoice);
        this.props.toggleInvoiceSideBar("edit")
       }}  label={"Edit"} customClass={["button-4"]}  />
-      <Button  label={"Delete"}  customClass={["button-5"]}  />
+      <Button  label={"Delete"}  onClick={()=>this.setShowDeleteDialog(true)}  customClass={["button-5"]}  />
       
       {
       invoice.status !== 'paid'? <Button onClick={this.markAsPaid}  label={"Mark as Paid"} customClass={["default"]}  />
@@ -57,6 +69,15 @@ class InvoiceView extends React.Component<Prop, State> {
      </>
     return (
         <div className='content'> 
+        <Dialog 
+        title='Confirm Deletion' 
+        description={`Are you sure you want to delete invoice #${invoice.id}? This action cannot be undone.`}  
+        show={this.state.showDeleteDialog} 
+        bottom={ <>
+                <Button  type="button" onClick={()=>this.setShowDeleteDialog(false)}   label={"Cancel"} customClass={["button-4"]}   />
+                <Button  type="button"  label={"Delete"} customClass={["button-5"]} onClick={()=>0}  /> </>}
+        />
+
         <Navigation path="/" label="Go Back" />
         <InvoiceHeader customClass="invoice-view-header" left={headerLeft}  right={headerRight}  />
         <InvoiceViewOne invoice={invoice}/>
