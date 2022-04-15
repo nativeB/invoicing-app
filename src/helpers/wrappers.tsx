e0a79ea5a86d11fc6252254782e573ae10f8f4f0
quincy <qhutchison@infonaligy.com>
2022-04-15 16:35:48 +0000

    Updates

--- src/helpers/wrappers.tsx
+++
@@ -1,9 +0,0 @@
-import { useParams, useNavigate } from "react-router-dom";
-
-export function withParams(Component: any) {
-    return (props: JSX.IntrinsicAttributes) => <Component {...props} params={useParams()} />;
-}
-
-export function withNavigate(Component: any) {
-    return (props: JSX.IntrinsicAttributes) => <Component {...props} navigate={useNavigate()} />;
-}
